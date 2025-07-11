17a94d483406c75a2aeb93cd0d14bc7f7d39ab64
Ben Williams <ben.williams.20@ucl.ac.uk>
2025-07-11 15:32:07 +0100

    rm copied folder

--- add_this/setup.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
-chrome_options = Options()
-chrome_options.add_argument("--user-data-dir=/Users/aparna/selenium-profile")
-driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://chat.openai.com/")
-
